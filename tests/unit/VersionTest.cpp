ab0f7885fa77b54b7e0c519b496e9e5566169268
Keeththigan <keeththi2003@gmail.com>
2026-07-06 02:14:25 +0530

    feat: add unit test for Version class to verify current version retrieval

---
+++ tests/unit/VersionTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include <gridx/matching/common/Version.hpp>
+
+TEST(VersionTest, ReturnsCurrentVersion)
+{
+    EXPECT_EQ(
+        gridx::matching::common::Version::getVersion(),
+        "0.1.0");
+}
