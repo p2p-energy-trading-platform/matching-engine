7cdef6877e200b0618c68b1fb28f12658195e73c
Keeththigan <keeththi2003@gmail.com>
2026-07-06 02:14:05 +0530

    feat: implement Version class with version retrieval method

---
+++ src/common/Version.cpp
@@ -0,0 +1,10 @@
+#include <gridx/matching/common/Version.hpp>
+
+namespace gridx::matching::common {
+
+std::string Version::getVersion()
+{
+    return "0.1.0";
+}
+
+} // namespace gridx::matching::common
