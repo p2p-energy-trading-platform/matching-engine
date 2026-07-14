48801b6eae15111837aa91520671b4e523307599
Keeththigan <keeththi2003@gmail.com>
2026-07-14 15:47:03 +0530

    feat: add GridTransferRule struct to domain model

---
+++ include/gridx/matching/domain/GridTransferRule.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "gridx/matching/common/Types.hpp"
+
+namespace gridx::matching
+{
+
+struct GridTransferRule
+{
+    GridZoneId sellerGridZone;
+    GridZoneId buyerGridZone;
+
+    bool allowed;
+
+    GridFee gridFee;
+
+    GridRuleVersion version;
+};
+
+} // namespace gridx::matching
