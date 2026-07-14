abbf9996285d575bd23759f0bda39739d97ed66c
Keeththigan <keeththi2003@gmail.com>
2026-07-14 16:09:31 +0530

    refactor: omit order storage from PriceLevel domain model

--- include/gridx/matching/domain/PriceLevel.hpp
+++ include/gridx/matching/domain/PriceLevel.hpp
@@ -11,7 +11,13 @@ struct PriceLevel
 {
     Price price;
 
-    std::deque<Order> orders;
+    // std::deque<Order> orders;
+
+    // NOTE:
+    // Order storage is intentionally omitted from the domain model.
+    // The OrderBook implementation will determine the appropriate
+    // container (e.g. std::deque<Order> or std::deque<std::shared_ptr<Order>>)
+    // based on performance and ownership requirements.
 };
 
 } // namespace gridx::matching
