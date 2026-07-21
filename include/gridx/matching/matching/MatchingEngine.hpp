104a270e9ad752a6c618c49aa7c3833a2ba21a48
Keeththigan <keeththi2003@gmail.com>
2026-07-21 11:08:06 +0530

    feat: add MatchingEngine header with class definition and constructor

---
+++ include/gridx/matching/matching/MatchingEngine.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+
+#include "gridx/matching/domain/Order.hpp"
+#include "gridx/matching/domain/Trade.hpp"
+
+namespace gridx::matching
+{
+
+class SameZoneMatcher;
+class CrossZoneMatcher;
+class TradeManager;
+class MarketBook;
+
+class MatchingEngine
+{
+public:
+    MatchingEngine(
+        SameZoneMatcher& sameZoneMatcher,
+        CrossZoneMatcher& crossZoneMatcher,
+        TradeManager& tradeManager,
+        MarketBook& marketBook);
+
+    MatchingEngine(const MatchingEngine&) = delete;
+    MatchingEngine& operator=(const MatchingEngine&) = delete;
+    MatchingEngine(MatchingEngine&&) = delete;
+    MatchingEngine& operator=(MatchingEngine&&) = delete;
+
+    ~MatchingEngine() = default;
+
+    [[nodiscard]]
+    std::vector<Trade> process(Order incomingOrder);
+
+private:
+    SameZoneMatcher& m_sameZoneMatcher;
+    CrossZoneMatcher& m_crossZoneMatcher;
+    TradeManager& m_tradeManager;
+    MarketBook& m_marketBook;
+};
+
+} // namespace gridx::matching
