15f2953871815f138b28d880bba20d4716608a3c
Keeththigan <keeththi2003@gmail.com>
2026-07-21 09:22:57 +0530

    doc: add comments to ZoneOrderBook methods

--- src/orderbook/ZoneOrderBook.cpp
+++ src/orderbook/ZoneOrderBook.cpp
@@ -7,7 +7,9 @@ ZoneOrderBook::ZoneOrderBook(GridZoneId gridZone)
     : gridZone_(gridZone)
 {
 }
-
+/**
+ * Inserts an order into the appropriate book based on its side.
+ */
 void ZoneOrderBook::addOrder(const OrderPtr& order)
 {
     if (order->side == Side::Buy)
@@ -20,26 +22,41 @@ void ZoneOrderBook::addOrder(const OrderPtr& order)
     }
 }
 
+/**
+ * Returns the grid zone associated with this order book.
+ */
 GridZoneId ZoneOrderBook::gridZone() const noexcept
 {
     return gridZone_;
 }
 
+/**
+ * Returns the buy order book.
+ */
 BuyBook& ZoneOrderBook::buyBook() noexcept
 {
     return buyBook_;
 }
 
+/**
+ * Returns the buy order book (const version).
+ */
 const BuyBook& ZoneOrderBook::buyBook() const noexcept
 {
     return buyBook_;
 }
 
+/**
+ * Returns the sell order book.
+ */
 SellBook& ZoneOrderBook::sellBook() noexcept
 {
     return sellBook_;
 }
 
+/**
+ * Returns the sell order book (const version).
+ */
 const SellBook& ZoneOrderBook::sellBook() const noexcept
 {
     return sellBook_;
