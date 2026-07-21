fe1d05af7376d8d17d85cc56984032b02486cb2f
Keeththigan <keeththi2003@gmail.com>
2026-07-21 09:57:24 +0530

    test: add new tests for market book functionality

--- tests/unit/orderbook/MarketBookTest.cpp
+++ tests/unit/orderbook/MarketBookTest.cpp
@@ -40,3 +40,99 @@ protected:
         return order;
     }
 };
+
+TEST_F(MarketBookTest, StoresMarketId)
+{
+    EXPECT_EQ(marketBook.marketId(), MarketId{});
+}
+
+TEST_F(MarketBookTest, CreatesZoneBookWhenFirstOrderIsAdded)
+{
+    marketBook.addOrder(
+        makeOrder(1, 1, Side::Buy, 100));
+
+    ASSERT_EQ(marketBook.zoneOrderBooks().size(), 1);
+
+    auto it = marketBook.zoneOrderBooks().find(1);
+
+    ASSERT_NE(it, marketBook.zoneOrderBooks().end());
+
+    EXPECT_EQ(it->second.gridZone(), 1);
+}
+
+TEST_F(MarketBookTest, ReusesExistingZoneBook)
+{
+    marketBook.addOrder(
+        makeOrder(1, 1, Side::Buy, 100));
+
+    marketBook.addOrder(
+        makeOrder(2, 1, Side::Sell, 105));
+
+    EXPECT_EQ(marketBook.zoneOrderBooks().size(), 1);
+
+    auto it = marketBook.zoneOrderBooks().find(1);
+
+    ASSERT_NE(it, marketBook.zoneOrderBooks().end());
+
+    EXPECT_FALSE(it->second.buyBook().empty());
+    EXPECT_FALSE(it->second.sellBook().empty());
+}
+
+TEST_F(MarketBookTest, CreatesSeparateZoneBooks)
+{
+    marketBook.addOrder(
+        makeOrder(1, 1, Side::Buy, 100));
+
+    marketBook.addOrder(
+        makeOrder(2, 2, Side::Buy, 100));
+
+    marketBook.addOrder(
+        makeOrder(3, 3, Side::Sell, 100));
+
+    EXPECT_EQ(marketBook.zoneOrderBooks().size(), 3);
+}
+
+TEST_F(MarketBookTest, RoutesOrdersToCorrectZoneBook)
+{
+    marketBook.addOrder(
+        makeOrder(1, 1, Side::Buy, 100));
+
+    marketBook.addOrder(
+        makeOrder(2, 2, Side::Sell, 105));
+
+    auto zone1 = marketBook.zoneOrderBooks().find(1);
+    auto zone2 = marketBook.zoneOrderBooks().find(2);
+
+    ASSERT_NE(zone1, marketBook.zoneOrderBooks().end());
+    ASSERT_NE(zone2, marketBook.zoneOrderBooks().end());
+
+    EXPECT_FALSE(zone1->second.buyBook().empty());
+    EXPECT_TRUE(zone1->second.sellBook().empty());
+
+    EXPECT_TRUE(zone2->second.buyBook().empty());
+    EXPECT_FALSE(zone2->second.sellBook().empty());
+}
+
+TEST_F(MarketBookTest, ReturnsExistingZoneOrderBook)
+{
+    marketBook.addOrder(
+        makeOrder(1, 1, Side::Buy, 100));
+
+    auto& zone1 = marketBook.zoneOrderBook(1);
+
+    EXPECT_EQ(zone1.gridZone(), 1);
+
+    EXPECT_FALSE(zone1.buyBook().empty());
+}
+
+TEST_F(MarketBookTest, CreatesZoneBookOnDemand)
+{
+    auto& zone = marketBook.zoneOrderBook(5);
+
+    EXPECT_EQ(zone.gridZone(), 5);
+
+    EXPECT_TRUE(zone.buyBook().empty());
+    EXPECT_TRUE(zone.sellBook().empty());
+
+    EXPECT_EQ(marketBook.zoneOrderBooks().size(), 1);
+}
