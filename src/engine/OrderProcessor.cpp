174200b2f1b5ba47477bae655acd3823f7000c84
Keeththigan <keeththi2003@gmail.com>
2026-08-07 10:31:47 +0530

    feat: refactor OrderProcessor to include additional dependencies and improve process method

--- src/engine/OrderProcessor.cpp
+++ src/engine/OrderProcessor.cpp
@@ -1,30 +1,42 @@
 #include "gridx/matching/engine/OrderProcessor.hpp"
 
 #include <spdlog/spdlog.h>
-#include <utility>
+
 #include "gridx/matching/adapters/kafka/ProtobufCodec.hpp"
 #include "gridx/order/v1/order_events.pb.h"
 
 namespace gridx::matching::engine {
 
-OrderProcessor::OrderProcessor(adapters::kafka::OrderEventMapper mapper,
-                               validation::OrderValidator& validator)
-    : mapper_{std::move(mapper)}, validator_{validator} {}
+OrderProcessor::OrderProcessor(adapters::kafka::OrderEventMapper& orderEventMapper,
+                               validation::OrderValidator& validator,
+                               matching::MatchingEngine& matchingEngine,
+                               orderbook::OrderBookUpdater& orderBookUpdater,
+                               adapters::kafka::MatchingResultPublisher& matchingResultPublisher)
+    : m_orderEventMapper(orderEventMapper)
+    , m_validator(validator)
+    , m_matchingEngine(matchingEngine)
+    , m_orderBookUpdater(orderBookUpdater)
+    , m_matchingResultPublisher(matchingResultPublisher) {}
 
-void OrderProcessor::process(const std::span<const std::byte> payload) {
+void OrderProcessor::process(std::span<const std::byte> payload) {
     const auto event =
-        adapters::kafka::ProtobufCodec::deserialize<order::v1::OrderAccepted>(payload);
+        adapters::kafka::ProtobufCodec::deserialize<gridx::order::v1::OrderAccepted>(payload);
 
     if (!event) {
-        spdlog::debug("Invalid order payload");
+        spdlog::debug("Received invalid order payload");
         return;
     }
 
-    const auto order = mapper_.toDomain(*event);
+    const Order order = m_orderEventMapper.toDomain(*event);
 
-    validator_.validate(order);
+    m_validator.validate(order);
 
-    // TODO: submit order in matching pipeline
+    const MatchingResult result = m_matchingEngine.match(order);
+
+    // TODO:
+    // Apply MatchingResult to MarketBook.
+
+    m_matchingResultPublisher.publish(result);
 }
 
 }  // namespace gridx::matching::engine
