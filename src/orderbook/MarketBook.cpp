4ae94e53c1ac47d4012adcd0c324203356cc6bd6
Keeththigan <keeththi2003@gmail.com>
2026-08-13 22:55:27 +0530

    feat: implement mutex locking in MarketBook for thread safety

--- src/orderbook/MarketBook.cpp
+++ src/orderbook/MarketBook.cpp
@@ -1,5 +1,8 @@
 #include "gridx/matching/orderbook/MarketBook.hpp"
 
+#include <mutex>
+#include <shared_mutex>
+
 namespace gridx::matching::orderbook {
 
 MarketBook::MarketBook(const MarketId& marketId) : marketId_(marketId) {}
@@ -8,7 +11,9 @@ MarketBook::MarketBook(const MarketId& marketId) : marketId_(marketId) {}
  * Inserts an order into the appropriate Zone Order Book.
  */
 void MarketBook::addOrder(const OrderPtr& order) {
-    zoneOrderBook(order->gridZone).addOrder(order);
+    std::unique_lock lock(mutex_);
+
+    zoneOrderBookUnlocked(order->gridZone).addOrder(order);
 }
 
 /**
@@ -23,21 +28,43 @@ const MarketId& MarketBook::marketId() const noexcept {
  * Creates one if it does not already exist.
  */
 ZoneOrderBook& MarketBook::zoneOrderBook(GridZoneId gridZone) {
+    std::unique_lock lock(mutex_);
+
+    return zoneOrderBookUnlocked(gridZone);
+}
+
+const ZoneOrderBook* MarketBook::findZoneOrderBook(GridZoneId gridZone) const {
+    std::shared_lock lock(mutex_);
+
+    return findZoneOrderBookUnlocked(gridZone);
+}
+
+std::size_t MarketBook::zoneOrderBookCount() const {
+    std::shared_lock lock(mutex_);
+
+    return zoneOrderBooks_.size();
+}
+
+bool MarketBook::hasZoneOrderBook(GridZoneId gridZone) const {
+    std::shared_lock lock(mutex_);
+
+    return zoneOrderBooks_.find(gridZone) != zoneOrderBooks_.end();
+}
+
+ZoneOrderBook& MarketBook::zoneOrderBookUnlocked(GridZoneId gridZone) {
     auto [it, inserted] = zoneOrderBooks_.try_emplace(gridZone, gridZone);
 
     return it->second;
 }
 
-/**
- * Returns all Zone Order Books.
- */
+const ZoneOrderBook* MarketBook::findZoneOrderBookUnlocked(GridZoneId gridZone) const noexcept {
+    const auto iterator = zoneOrderBooks_.find(gridZone);
 
-std::map<GridZoneId, ZoneOrderBook>& MarketBook::zoneOrderBooks() noexcept {
-    return zoneOrderBooks_;
-}
+    if (iterator == zoneOrderBooks_.end()) {
+        return nullptr;
+    }
 
-const std::map<GridZoneId, ZoneOrderBook>& MarketBook::zoneOrderBooks() const noexcept {
-    return zoneOrderBooks_;
+    return &iterator->second;
 }
 
 }  // namespace gridx::matching::orderbook
